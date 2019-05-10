0cc3dbd179b8f226606b7b01e083294fee2eca6a
Vladimir Nenashev <vn@payneteasy.com>
2019-05-10 14:03:30 +0300

    added index page

---
+++ src/main/java/com/nenashev/oauthdemo/oauthdemoclient/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.nenashev.oauthdemo.oauthdemoclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class MainController {
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "index";
+    }
+}
