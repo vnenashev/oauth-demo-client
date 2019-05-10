c8dfcae8a15b36f1846810cdfdebddd410ee8817
Vladimir Nenashev <vn@payneteasy.com>
2019-05-10 12:12:37 +0300

    added initial set of files

---
+++ src/main/java/com/nenashev/oauthdemo/oauthdemoclient/OauthDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.nenashev.oauthdemo.oauthdemoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoClientApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(OauthDemoClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nenashev/oauthdemo/oauthdemoclient/OauthDemoClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nenashev.oauthdemo.oauthdemoclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthDemoClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
